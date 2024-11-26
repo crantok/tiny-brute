166d62fb50c7a1f19411a862eaaff0c6f6a43823
J Hellings <justin.hellings@gmail.com>
2024-11-26 21:25:29 +0000

    Added COMMANDS and made PROJECT_DIRECTORY an option argument

--- tbrute.rb
+++ tbrute.rb
@@ -8,6 +8,9 @@ require 'pathname'
 
 # APP_NAME = "Tiny Brute"
 PAGE_FILENAME_EXTENSION = "page.brute"
+PROJECT_DIR_ARG_NAME = "PROJECT_DIRECTORY"
+
+config = { project_dir: Dir.pwd }
 
 
 ############################
@@ -23,64 +26,86 @@ logger.level = Logger::DEBUG
 # Processing options first because OptionParser#parse! conveniently removes
 # all options and their associated arguments from ARGV.
 
-options = {}
 OptionParser.new do |parser|
 
   # The only permitted command line option is a request for usage instructions
   # triggered by -h or --help
-  parser.banner = "Usage: tbrute.rb [PROJECT_DIRECTORY]
-arguments:
-    PROJECT_DIRECTORY  root directory of the project
-                       default = curent working directory (usually the
-                       directory that this script was called from)
-                       Project config file (if any) should be placed here."
+  parser.banner =
+    "Usage: #{File.basename(__FILE__)} [OPTIONS] COMMAND
 
-  # The second argument passed to OptionParser#on is parsed to determine
-  # whether there is a required or [optional] argument.
-  # Not the most self-documenting interface.
-  # Example:
-  # parser.on( "-t", "--test [OPTIONAL_ARG]", "testing the options parser") do |t|
-  #   options[:test] = t
-  # end
+COMMANDS:
+    generate | gen (default command)
+        generate site in the work-in-progress directory
+        (default directory: #{PROJECT_DIR_ARG_NAME}/output)
+    clean-and-generate | clgen
+        delete contents of the work-in-progress directory and then generate
+        site there
+    publish | pub
+        generate site in a new, (probably) uniquely-named directory
+
+OPTIONS:"
+
+  parser.on(
+    "-d #{PROJECT_DIR_ARG_NAME}", # mandatory argument when this option is used
+    "--project-dir",
+    "The directory of the project to process (default = present working directory)"
+  ) do | dir |
+    config[:project_dir] = File.expand_path( dir )
+  end
 
   # Can change option parsing error messages by rescuing errors...
   # rescue OptionParser::InvalidOption => e
   # rescue OptionParser::InvalidArgument => e
+
 end.parse!
+# Only positional arguments are left in ARGV.
+# OptionParser has removed all options/switches and their arguments.
+
+logger.info( "Generating static site from project at " + config[:project_dir] )
+
 
 
 ###########################
-# Process postional command line arguments: project_dir
-#
-# Allow override of project directory via command line argument.
+# Process command
+# 
 
-if ( ARGV.length > 1 )
-  logger.error( "Too many arguments. Expected PROJECT_DIRECTORY or nothing." )
+if ARGV.length > 1
+  logger.error( "Too many arguments. Expected COMMAND or nothing." )
   abort()
-elsif ( ARGV.length == 1 )
-  project_dir = File.expand_path( ARGV[0] )
+elsif ARGV.length == 1
+  command = ARGV[0]
 else
-  project_dir = Dir.pwd  # default: current working directory 
+  command = "gen"
 end
 
-logger.info("Generating static site from project at " + project_dir)
 
 
 ############################
 # Define default config
 #
 
-config = {
-  project_dir: project_dir,
-  input_dir: File.join( project_dir, 'input' ),
-  global_injectors_dir: File.join( project_dir, 'globals' ),
+config[:input_dir] = File.join( config[:project_dir], 'input' )
+config[:global_injectors_dir] = File.join( config[:project_dir], 'globals' )
 
-  # Including microseconds in the output dir name in case this script is run
-  # more than once in the same second.
-#  output_dir: File.join( project_dir, Time.now.strftime('%Y-%m-%d--%H-%M--%S.%N') )
-  output_dir: File.join( project_dir, 'output' )
-}
+config[:output_dir] =
+  if [ 'gen', 'generate', 'clgen', 'clean-and-generate' ].include? command
+    File.join( config[:project_dir], 'output' )
 
+  else  # [ 'pub', 'publish' ].include? command
+    # Including microseconds in the output dir name in case this script is run
+    # more than once in the same second.
+    File.join( project_dir, Time.now.strftime('%Y-%m-%d--%H-%M--%S.%N') )
+  end
+
+
+
+##########################
+# Clean output directory if appropriate.
+#
+
+if [ 'clgen', 'clean-and-generate' ].include? command
+  # TO DO: delete contents of project-dir
+end
 
 
 ##########################
@@ -122,13 +147,13 @@ end
 # 2) require all ruby files in the global_injectors_dir
 #    on require, each ruby file should automatically register its plugin(s)
 
-Dir[config[:global_injectors_dir]+"/*.rb"].each do |file|
+Dir[ File.join( config[:global_injectors_dir], "*.rb" ) ].each do |file|
   require file
   logger.info("Required plugin file: " + file)
 end
 
- 
-# 4) (Optional idea) Each plugin defines which page_data keys it wants and it
+
+# 3) (Optional idea) Each plugin defines which page_data keys it wants and it
 #    can only receive those keys. That increases the liklihood of the plugin
 #    code listing all the keys it uses in one place. (Although it might list
 #    keys that the code no longer uses.)
@@ -144,16 +169,32 @@ logger.debug( Plugins.inflate_page( {main_content:"Foo"}, "<html><body><div id='
 
 in_dir_path = Pathname.new config[:input_dir]
 
-# For each page file
-Dir.glob( File.join( config[:input_dir], "**", "*." + PAGE_FILENAME_EXTENSION ) ) do | filename |
+# # For each page file
+# Dir.glob( File.join( config[:input_dir], "**", "*." + PAGE_FILENAME_EXTENSION ) ) do | filename |
 
-  page_properties = TomlRB.load_file(filename)
-  markup = Plugins.inflate_page(
-    page_properties, File.read( page_properties["template"] ) )
+# For each file in input directory
+Dir.glob( File.join( config[:input_dir], "**" ) ) do | filename |
 
-  in_file_path = Pathname.new filename
-  file_rel_path = in_file_path.relative_path_from in_dir_path
-  output_filename = File.join( config[:output_dir], file_rel_path )
-  File.write( output_filename, markup )
+  if File.directory?( filename )
+    # make same directory in output_dir
+
+
+  elsif filename.end_with?( PAGE_FILENAME_EXTENSION )
+    # Load the page properties from the input dir
+    page_properties = TomlRB.load_file(filename)
+
+    # create the page markup
+    markup = Plugins.inflate_page(
+      page_properties, File.read( page_properties["template"] ) )
+
+    # save the page to the output dir
+    in_file_path = Pathname.new filename
+    file_rel_path = in_file_path.relative_path_from in_dir_path
+    output_filename = File.join( config[:output_dir], file_rel_path )
+    File.write( output_filename, markup )
+
+  else
+    # copy file to destination
+
+  end
 end
-
